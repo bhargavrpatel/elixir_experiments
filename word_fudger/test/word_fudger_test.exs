a5ce341ca12c3ae86905ceb003ab17e5030459d6
Bhargav R. Patel <ziikutv@gmail.com>
2016-08-31 20:09:56 +0000

    Add failing tests for unvoweler

--- word_fudger/test/word_fudger_test.exs
+++ word_fudger/test/word_fudger_test.exs
@@ -2,10 +2,7 @@ defmodule WordFudgerTest do
   use ExUnit.Case
   doctest WordFudger
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-
+  #Upcase method tests
   test "it should not touch single word strings" do
     assert WordFudger.upcase("foo") == "foo"
   end
@@ -16,6 +13,19 @@ defmodule WordFudgerTest do
 
   test "it should upcase every other word" do
     assert WordFudger.upcase("foo bar buzz qux") == "foo BAR buzz QUX"
+  end
+
+  #Unvowel method tests
+  test "it should not unvowel single word strings" do
+    assert WordFudger.unvowel("foo") == "foo"
+  end
+
+  test "it should unvowel second word in two word string" do
+    assert WordFudger.unvowel("foo bar") == "foo br"
+  end
+
+  test "it should unvowel every other word" do
+    assert WordFudger.unvowel("foo bar buzz qux") == "foo br buzz qx"
   end
 
 end
