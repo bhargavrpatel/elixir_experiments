3a6823050daadb6bc687d36843c75a34259a43af
Bhargav R. Patel <ziikutv@gmail.com>
2016-08-31 19:56:31 +0000

    Add failing tests for upcase method

--- word_fudger/test/word_fudger_test.exs
+++ word_fudger/test/word_fudger_test.exs
@@ -5,4 +5,17 @@ defmodule WordFudgerTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "it should not touch single word strings" do
+    assert WordFudger.upcase("foo") == "foo"
+  end
+
+  test "it should upcase second word in two word string" do
+    assert WordFudger.upcase("foo bar") == "foo BAR"
+  end
+
+  test "it should upcase every other word" do
+    assert WordFudger.upcase("foo bar buzz qux") == "foo BAR buzz QUX"
+  end
+
 end
